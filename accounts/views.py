6a67373275ddd8b9db8df01f6ba1bb28df17fff4
Jonas Hessling <firstname.jonas@icloud.com>
2024-02-22 21:52:37 +0000

    Import generic class

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views import generic
 
 
 class SignUpView(generic.CreateView):
