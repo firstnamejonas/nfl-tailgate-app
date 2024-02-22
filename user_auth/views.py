c895332f441eb1c9ada6b7d773490802e611364c
Jonas Hessling <firstname.jonas@icloud.com>
2024-02-22 16:57:37 +0000

    Reset views file to restructure app

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,22 +1,3 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
+from django.shortcuts import render
 
 # Create your views here.
-
-def signup_view(request):
-    if request.method == 'POST':
-        pass
-    else:
-        return render(request, 'user_auth/signup.html')
-
-def login_view(request):
-    if request.method == 'POST':
-        pass
-    else:
-        return render(request, 'user_auth/login.html')
-
-def logout_view(request):
-    logout(request)
-    messages.success(request, 'You have been logged out successfully.')
-    return redirect('login')
