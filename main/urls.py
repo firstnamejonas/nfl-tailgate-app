e74711fdf12406cb1581e18a72d36123fec8bd93
Jonas Hessling <firstname.jonas@icloud.com>
2024-02-22 21:11:19 +0000

    Add home page url routing

--- main/urls.py
+++ main/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
