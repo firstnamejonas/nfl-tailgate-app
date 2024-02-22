c5dd4d4cddeb3c371bd7b60fd35ea58a72456174
Jonas Hessling <firstname.jonas@icloud.com>
2024-02-22 15:19:34 +0000

    Add user authentication views

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def signup_view(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'user_auth/signup.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'user_auth/login.html')
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have been logged out successfully.')
+    return redirect('login')
