ade95dbc82beb78b1f099e9b6e0f02475493beaf
Jonas Hessling <firstname.jonas@icloud.com>
2024-02-28 16:03:05 +0000

    Add model to create chatrooms

--- all_chatrooms/models.py
+++ all_chatrooms/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Room(models.Model):
+    chatroom_title = models.CharField(max_length=100, unique=True)
+    chatroom_image = models.ImageField(upload_to='room_images', null=True, blank=True)
+
+    def __str__(self):
+        return self.chatroom_title
