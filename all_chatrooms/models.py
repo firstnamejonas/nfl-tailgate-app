4bb63e2fdf27f1ee70ce178923bdf055fcd9dd92
Jonas Hessling <firstname.jonas@icloud.com>
2024-02-28 16:07:17 +0000

    Delete unnecessary import

--- all_chatrooms/models.py
+++ all_chatrooms/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
 class Room(models.Model):
     chatroom_title = models.CharField(max_length=100, unique=True)
     chatroom_image = models.ImageField(upload_to='room_images', null=True, blank=True)
