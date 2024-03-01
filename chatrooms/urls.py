a113639a1db5d2b9f3e60d7e67ce0049c4e56449
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-01 13:15:14 +0000

    Add url routing in chatroom app

--- chatrooms/urls.py
+++ chatrooms/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-
-from .views import room_list
-
+from . import views
 
 urlpatterns = [
-    path("chatrooms/", room_list.as_view(), name="chatrooms"),
+    path('', views.room_list, name='chatrooms'),
 ]
