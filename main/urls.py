e274e270fd1d6c50abcc91013b4b104b792d74c2
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-04 15:20:52 +0000

    Add url routing for userprofile app

--- main/urls.py
+++ main/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("chatrooms", include("chatrooms.urls")),
+    path("userprofile", include("userprofile.urls")),
 ]
