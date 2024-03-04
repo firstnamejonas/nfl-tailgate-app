ed71ab86977494cd976e2a841460d0819603de53
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-04 13:12:10 +0000

    Fix bug in model and make migrations

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
-from rooms.models import Team
+from cloudinary.models import CloudinaryField
+from chatrooms.models import Team
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture = CloudinaryField('image', default='placeholder', blank=True, null=True)
-    favorite_team = models.ForeignKey(Team, on_delete=SET_NULL, null=True, blank=True)
+    favorite_team = models.ForeignKey('chatrooms.Team', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
