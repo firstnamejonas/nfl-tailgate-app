78c35dd09bd2e67fcc8750810192d87281690d5f
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-04 13:01:39 +0000

    Add model for user profile page

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+from rooms.models import Team
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = CloudinaryField('image', default='placeholder', blank=True, null=True)
+    favorite_team = models.ForeignKey(Team, on_delete=SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
