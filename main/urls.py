30d909b9ddf68817d330f84aaa88c7e0a3123e7a
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-07 13:56:31 +0000

    Add urlpattern to include urls from chat app

--- main/urls.py
+++ main/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("chatrooms", include("chatrooms.urls")),
     path("userprofile", include("userprofile.urls")),
+    path("chat/", include("chat.urls")),
 ]
