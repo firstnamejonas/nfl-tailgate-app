51cbe72988e7d34696ebafe2f363e84156febc00
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-07 12:57:55 +0000

    Add first test model for messages to be send

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from chatrooms.models import Room
 
-# Create your models here.
+class Message(models.Model):
+    room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender.username} - {self.timestamp}'
