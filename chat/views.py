2e1a18f0c7efc42acfbbb782fd84518ca564a10a
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-08 12:28:44 +0000

    Add slug to function for room url

--- chat/views.py
+++ chat/views.py
@@ -4,14 +4,14 @@ from .models import Message
 from chatrooms.models import Room
 
 @login_required
-def chat_room(request, room_id):
-    room = get_object_or_404(Room, pk=room_id)
+def chat_room(request, room_slug):
+    room = get_object_or_404(Room, slug=room_slug)
     messages = Message.objects.filter(room=room).order_by('timestamp')
     
     if request.method == 'POST':
         content = request.POST.get('content')
         if content:
             Message.objects.create(room=room, sender=request.user, content=content)
-            return redirect('chat:chat_room', room_id=room_id)
+            return redirect('chat:chat_room', room_slug=room_slug)
     
     return render(request, 'chat.html', {'room': room, 'messages': messages})
