a1d62d111e096f0596a91488ca567907c70c5103
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-08 13:40:23 +0000

    Add delete, edit message pages urls

--- chat/urls.py
+++ chat/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'chat'
 urlpatterns = [
     path('<slug:room_slug>/', views.chat_room, name='chat_room'),
+    path('edit/<int:message_id>/', views.edit_message, name='edit_message'),
+    path('delete/<int:message_id>/', views.delete_message, name='delete_message'),
 ]
