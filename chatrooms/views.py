e8203b45dd7e47f613fa4c17e942b56d83456351
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-08 14:43:27 +0000

    Add user profile to function for favorite team chat

--- chatrooms/views.py
+++ chatrooms/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Room
+from userprofile.models import Profile
 
 def room_list(request):
     rooms = Room.objects.all()
     categories = set(room.category for room in rooms)
-    return render(request, 'chatrooms.html', {'rooms': rooms, 'categories': categories})
+    profile = Profile.objects.get(user=request.user)
+    return render(request, 'chatrooms.html', {'rooms': rooms, 'categories': categories, 'profile': profile})
