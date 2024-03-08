a7116e3a50fc9ddc0483b51caf69a9a0879656b4
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-08 12:16:39 +0000

    Add slug field for better chatroom urls

--- chatrooms/models.py
+++ chatrooms/models.py
@@ -11,8 +11,14 @@ class Team(models.Model):
 class Room(models.Model):
     CATEGORIES = (('Teams', 'Teams'), ('Fantasy Football & More', 'Fantasy Football & More'), ('Other Teams', 'Other Teams'))
     name = models.CharField(max_length=100, null=False, blank=False)
+    slug = models.SlugField(max_length=200, unique=True, default='')
     category = models.CharField(max_length=100, choices=CATEGORIES, null=False, blank=False)
     team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = generate_slug(self.name)
+        super(Room, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
