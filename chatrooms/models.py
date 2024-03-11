dbc6e24f0fb10f56fda347c31f598566a285628c
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-11 14:41:48 +0000

    Add comments for chatroom app models

--- chatrooms/models.py
+++ chatrooms/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 class Team(models.Model):
+    """
+    Summary:
+    Model for representing a team.
+
+    Attributes:
+    name (str): The name of the team.
+    logo (CloudinaryField): The logo of the team stored in Cloudinary.
+
+    Methods:
+    __str__ (str): Returns a string representation of the team.
+    """
     name = models.CharField(max_length=100)
     logo = CloudinaryField('image', default='placeholder')
 
@@ -9,6 +20,20 @@ class Team(models.Model):
         return self.name
 
 class Room(models.Model):
+    """
+    Summary:
+    Model for representing a chat room.
+
+    Attributes:
+    name (str): The name of the chat room.
+    slug (str): The slug of the chat room.
+    category (str): The category of the chat room.
+    team (Team): The team associated with the chat room.
+
+    Methods:
+    save: Overridden save method to generate a slug for the room if not provided.
+    __str__ (str): Returns a string representation of the chat room.
+    """
     CATEGORIES = (('Teams', 'Teams'), ('Fantasy Football & More', 'Fantasy Football & More'), ('Other Teams', 'Other Teams'))
     name = models.CharField(max_length=100, null=False, blank=False)
     slug = models.SlugField(max_length=200, unique=True, default='')
