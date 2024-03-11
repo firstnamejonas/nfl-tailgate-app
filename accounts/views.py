57dfa64a2fa36b203af4abe5982471881eaff9c2
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-11 14:30:42 +0000

    Add comments for signup views

--- accounts/views.py
+++ accounts/views.py
@@ -7,11 +7,28 @@ from django.contrib import messages
 
 
 class SignUpView(generic.CreateView):
+    """
+    Summary:
+    View class for user sign-up functionality.
+
+    Attributes:
+    form_class (UserCreationForm): Form class for user creation.
+    success_url (str): URL to redirect to upon successful user creation.
+    template_name (str): Template file for rendering the sign-up page.
+
+    Methods:
+    form_valid (HttpResponse): Handles valid form submission, displays success message and redirects user.
+
+    """
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
     def form_valid(self, form):
+        """
+        Function to show success message on Login-Page,
+        after successfull user signup!
+        """
         response = super().form_valid(form)
         messages.success(self.request, 'Your account has been created successfully. Please login.')
         return response
