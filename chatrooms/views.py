44a8ad81a5ea4483c73a5bbbc5383233aa92b5ae
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-13 15:41:56 +0000

    Add login required to chatrooms view

--- chatrooms/views.py
+++ chatrooms/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Room
 from userprofile.models import Profile
 
+@login_required
 def room_list(request):
     """
     Summary:
