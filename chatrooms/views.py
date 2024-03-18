0cba8122ba20c7377fb32ec07bb0b8969deac49f
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-18 10:05:42 +0000

    Fix bug of false favorite chatroom showing

--- chatrooms/views.py
+++ chatrooms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Room
 from userprofile.models import Profile
 
@@ -18,19 +19,28 @@ def room_list(request):
     The HTTP response object containing the rendered room list page.
     """
     profile = Profile.objects.get(user=request.user)
-    favorite_team = profile.favorite_team
+    if profile.favorite_team is not None:
+        favorite_team = profile.favorite_team
+    else:
+        favorite_team = None
+        messages.error(request, 'Choose a team!')
+
     rooms = Room.objects.all()
 
     favorite_room = None
     other_rooms = []
     for room in rooms:
-        if room.team == favorite_team:
+        if favorite_team is not None and room.team == favorite_team:
             favorite_room = room
         else:
             other_rooms.append(room)
 
     categories = set(room.category for room in other_rooms)
-    return render(request, 'chatrooms.html', {'favorite_room': favorite_room,
-                                              'other_rooms': other_rooms,
-                                              'categories': categories,
-                                              'profile': profile})
+    template = 'chatrooms.html'
+    context = {
+        'favorite_room': favorite_room,
+        'other_rooms': other_rooms,
+        'categories': categories,
+        'profile': profile
+    }
+    return render(request, template, context)
