d27c8f630b867fc0819be04c25039aa1485ff446
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-26 11:28:48 +0000

    Add python unit testing for view in userprofile

--- userprofile/tests.py
+++ userprofile/tests.py
@@ -16,3 +16,29 @@ class ProfileModelTest(TestCase):
         expected_string = 'testuser1'
         self.assertEqual(str(profile), expected_string)
 
+class ProfileViewsTest(TestCase):
+    def setUp(self):
+        """
+        Set up a test client and create necessary objects for testing.
+        """
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser2', password='password')
+        self.team = Team.objects.create(name='Test Team')
+        self.profile = self.user.profile
+
+    def test_user_profile_view(self):
+        """
+        Test the user profile view.
+        """
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('userprofile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_profile.html')
+
+    def test_user_profile_view_post(self):
+        """
+        Test the user profile view for POST request.
+        """
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('userprofile'), {'change_picture': '', 'picture': 'test.jpg'})
+        self.assertEqual(response.status_code, 302)
