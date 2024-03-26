cae3ec605a458e65238ff6441f1614bdbdca13ed
Jonas Hessling <firstname.jonas@icloud.com>
2024-03-26 11:28:23 +0000

    Add python unit testing for model in userprofile

--- userprofile/tests.py
+++ userprofile/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Profile
+from chatrooms.models import Team
+from .forms import ProfilePictureForm, FavoriteTeamForm
 
-# Create your tests here.
+class ProfileModelTest(TestCase):
+    def test_profile_str(self):
+        """
+        Checks whether the string representation of the profile is correct.
+        """
+        user = User.objects.create(username='testuser1')
+        team = Team.objects.create(name='Test Team')
+        profile = user.profile
+        expected_string = 'testuser1'
+        self.assertEqual(str(profile), expected_string)
+
